cb3fcb93af9084b57645dab1d450676695a16821
Bartosz Kowalski <c0vval@gmail.com>
2019-03-20 21:33:35 +0100

    little change in email

--- cypress/integration/!training/register_page.js
+++ cypress/integration/!training/register_page.js
@@ -2,7 +2,7 @@ describe('Register new user', function(){
     it.only('Correct one', function(){
         cy.visit('/register')
         let randomName = 'validname' + Math.floor(Math.random(1000))
-        let randomEmail = randomName + 'email@com'
+        let randomEmail = randomName + '@email.com'
         cy.get('[placeholder=Username]').type(randomName)
         cy.get('[placeholder=Email]').type(randomEmail)
         cy.get('[placeholder=Password]').type('no password')
